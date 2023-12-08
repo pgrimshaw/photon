d6c6cf77aeefd4f16f7d714c24d45aa3299c359c
pgrimshaw <pgrimshaw@quantisset.net>
2023-12-07 19:34:38 -0500

    Calculate volume
    Calculate volume (gallons) from distance, and include in published data.

--- sugarhouse/sugarhouse.ino
+++ sugarhouse/sugarhouse.ino
@@ -75,17 +75,21 @@ void loop() {
         tempF = tempC * 9.0 / 5.0 + 32.0;
     }
 
+    double gallon = 0;
     double rfDist = rangeFinder.getDistanceCM();
     double dist =  rfDist * tempCorrection(tempC);
     bool distValid = ((dist >= MIN_CM) && (dist <= MAX_CM));
     if(distValid) {
         cm = initializing ? dist : smooth(dist, cm);
+        gallon = (89 - cm) * 5.0 / 8.0;
     }
+    double gallon2 = 0;
     double rfDist2 = rangeFinder2.getDistanceCM();
     double dist2 =  rfDist2 * tempCorrection(tempC);
     bool dist2Valid = ((dist2 >= MIN_CM) && (dist2 <= MAX_CM));
     if(dist2Valid) {
         cm2 = initializing ? dist2 : smooth(dist2, cm2);
+        gallon2 = (89 - cm2) * 5.0 / 8.0;
     }
 
     initializing = false;
@@ -101,10 +105,12 @@ void loop() {
         }
         if(distValid) {
             sdata += ",\"2\": \"" + String(cm) + "\"";
+            sdata += ",\"4\": \"" + String(gallon) + "\"";
             //Particle.publish("distance", String(cm), PRIVATE);
         }
         if(dist2Valid) {
             sdata += ",\"3\": \"" + String(cm2) + "\"";
+            sdata += ",\"5\": \"" + String(gallon2) + "\"";
             //Particle.publish("dist2", String(cm2), PRIVATE);
         }
         sdata += " }";
